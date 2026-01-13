98fc5cd1dba1856cd171c9b4412ccfb4cfee2028
Yashwanthraja21 <125111549+Yashwanthraja21@users.noreply.github.com>
2026-01-13 18:21:04 +0530

    Doc: Adding the sum of an matrix

--- python/2D Array/sum_of_matrix.py
+++ python/2D Array/sum_of_matrix.py
@@ -3,16 +3,16 @@ col = int(input("Enter the number of columns: "))
 matrix1, matrix2, result = [], [], []
 
 for i in range(row):
-    values = list(map(int, input().split()))
+    values = list(map(int, input().split())) # Read first matrix
     matrix1.append(values)
 
 for i in range(row):
-    values = list(map(int, input().split()))
-    matrix2.append(values)
+    values = list(map(int, input().split())) # Read second matrix
+    matrix2.append(values) 
 
 for i in range(row):
-    temp = []
+    temp = [] # Temporary list to store sum of each row
     for j in range(col):
-        temp.append(matrix1[i][j] + matrix2[i][j])
-    result.append(temp)
-print(result)
+        temp.append(matrix1[i][j] + matrix2[i][j]) # Sum corresponding elements
+    result.append(temp) # Append the summed row to result
+print(result) 
